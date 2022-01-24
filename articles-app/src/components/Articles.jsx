b59fc4e1562bc6474d1ef604f436124fdcd80fb6
Kerry Clements <kerryclements@Kerrys-Air.local.tld>
2022-01-24 14:32:53 +0000

    positioning of items in the grid

--- articles-app/src/components/Articles.jsx
+++ articles-app/src/components/Articles.jsx
@@ -10,26 +10,28 @@ export const Article = (props) => {
     // eslint-disable-next-line
         <article className={"article" + " " + ((props.color || "blue") + articleStyle)}>
 
-                    <h2 className={"small"}>{props.title}</h2>
-                    <h4 className={"big"}>{props.subtitle}</h4>
+                    <h2 className={"small-heading"}>{props.title}</h2>
+                    <h4 className={"large-heading"}>{props.subtitle}</h4>
                     <p>{props.content}</p>
-                    <div className={"button-container"}>
-                        <button className={"button-color"} onClick={() => {
-                            console.log(props.title + " " + props.subtitle)
-                            }}>Console.log title and subtitle
-                        </button>
-                        <button className={"button-color"} onClick={() => {
-                            const newStyle = getClassExtension(articleStyle)
-                            changeStyle(newStyle)
-                             }}>Toggle Colours!
-                        </button>
-                        <Button clickHandler={()=>{
-                            const newStyle = getClassExtension(articleStyle)
-                            changeStyle(newStyle)
-                            }}
-                            text={"Toggle Colours!"}
-                        />
-                    </div>
+                    <section className="buttons-box">
+                        <div className={"button-container"}>
+                            <button className={"button-color"} onClick={() => {
+                                console.log(props.title + " " + props.subtitle)
+                                }}>Console.log title and subtitle
+                            </button>
+                            <button className={"button-color"} onClick={() => {
+                                const newStyle = getClassExtension(articleStyle)
+                                changeStyle(newStyle)
+                                }}>Toggle Colours!
+                            </button>
+                            <Button clickHandler={()=>{
+                                const newStyle = getClassExtension(articleStyle)
+                                changeStyle(newStyle)
+                                }}
+                                text={"Toggle Colours!"}
+                            />
+                        </div>
+                    </section>
                 </article>
     );
 }
