c9068eeb50f6b8e1f30af28f2f7cc1629e659776
Kerry Clements <kerryclements@Kerrys-Air.local.tld>
2022-01-24 13:47:43 +0000

    refactored to make styling changes

--- articles-app/src/App.js
+++ articles-app/src/App.js
@@ -1,60 +1,11 @@
 
 import './App.css';
-import {Article} from './components/Articles';
+import { ArticlesContainer } from './components/ArticlesContainer.jsx';
 
 function App() {
   return (
       <div className="App">
-          <Article
-              color={"red"}
-              title={"this is the perfect title"}
-              subtitle={"foobar"}
-              content={"Hello. tijkukghakhkj ih iuay hiourhku iuryiukyhiuayuyru u yiurtujargiuhu jkh" +
-                  "              kjhkuhkujah iljiojo  oij iu ijiou jiluoijiou  this i the vesrtt way o doing this typing," +
-                  "                  that I know of" +
-                  "                  welcome to our world thit sis the stes palc 5to liv in othe colare system"}
-          />
-          <Article
-              color={"orange"}
-              title={"second title"}
-              subtitle={"holy moley guacamole"}
-              content={"Hello. tijkukghakhkj ih iuay hiourhku iuryiukyhiuayuyru u yiurtujargiuhu jkh" +
-                  "kjhkuhkujah iljiojo  oij iu ijiou jiluoijiou  this i the vesrtt way o doing this typing," +
-                  "that I know of" +
-                  "welcome to our world thit sis the stes palc 5to liv in othe colare system"}
-
-          />
-          <Article
-              color={"yellow"}
-              title={"third title"}
-              subtitle={"who knows?"}
-              content={"kjhkuhkujah iljiojo  oij iu ijiou jiluoijiou  this i the vesrtt way o doing this typing," +
-                  "that I know of" +
-                  "welcome to our world thit sis the stes palc 5to liv in othe colare system"}
-          />
-          <Article
-              color={"green"}
-              title={"4th title"}
-              subtitle={"who cares?"}
-              content={"kjhkuhkujah iljiojo  oij iu ijiou jiluoijiou  this i the vesrtt way o doing this typing," +
-                  "that I know of" +
-                  "welcome to our world thit sis the stes palc 5to liv in othe colare system"}
-          />
-          <Article
-              color={"blue"}
-              title={"5th title"}
-              subtitle={"llllllaaaa?"}
-              content={"kjhkuhkujah iljiojo  oij iu ijiou jiluoijiou  this i the vesrtt way o doing this typing," +
-                  "that I know of" +
-                  "welcome to our world thit sis the stes palc 5to liv in othe colare system"}
-          />
-        <Article
-            color={"pink"}
-            title={"6th title"}
-            subtitle={"slither slither snakey snake"}
-            content={"kjogauoij"+
-                "welcome to our world thit sis the stes palc 5to liv in othe colare system"}
-        />
+          <ArticlesContainer/>
       </div>
   );
 }
