e9897cf93088a2e14bca06d24c8f8aa369e0810a
Kerry Clements <kerryclements@Kerrys-Air.local.tld>
2022-01-25 15:06:06 +0000

    made more mobile responsive. Would like to fit cells of grid to the content, stetch when more,

--- articles-app/src/components/Articles.jsx
+++ articles-app/src/components/Articles.jsx
@@ -8,25 +8,30 @@ export const Article = (props) => {
 
     return(
         <article className={"article " + ((props.color || "blue") + articleStyle)}>
-                    <h2 className={"small-heading"}>{props.title}</h2>
-                    <h4 className={"large-heading"}>{props.subtitle}</h4>
-                    <p className="content-styling">{props.content}</p>
-                        <div className={"button-container"}>
-                            <Button clickHandler={() => {
-                                console.log(props.title + " " + props.subtitle)
-                                }}
-                                buttonClasses={"button-styling " + ((props.color || "blue") + articleStyle)}
-                                text={"Console.log title and subtitle"}
-                            />
-                            <Button clickHandler={() => {
-                                const newStyle = getClassExtension(articleStyle)
-                                changeStyle(newStyle)
-                                }}
-                                buttonClasses={"button-styling " + ((props.color || "blue") + articleStyle)}
-                                text={"Toggle Colours!"}
-                            />
-                        </div>
+            <section>
+                <h2 className={"small-heading"}>{props.title}</h2>
+                <h4 className={"large-heading"}>{props.subtitle}</h4>
+                <p className="content-styling">{props.content}</p>
+            </section>
+            <section className="centre">
+                <div className={"button-container"}>
+                    <Button clickHandler={() => {
+                        console.log(props.title + " " + props.subtitle)
+                        }}
+                        buttonClasses={"button-styling " + ((props.color || "blue") + articleStyle)}
+                        text={"Console.log title and subtitle"}
+                    />
+                    <Button clickHandler={() => {
+                        const newStyle = getClassExtension(articleStyle)
+                        changeStyle(newStyle)
+                        }}
+                        buttonClasses={"button-styling " + ((props.color || "blue") + articleStyle)}
+                        text={"Toggle Colours!"}
+                    />
+                </div>
+            </section>
 
-                </article>
+
+        </article>
     );
 }
